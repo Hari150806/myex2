86639b477c2eded31639e16b40b41a38d00b76aa
Hariharan P <pchari217@gmail.com>
2025-07-24 16:17:21 +0530

    first commit

--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Visitor(models.Model):
+    visitor_id = models.PositiveIntegerField(unique=True)  # Unique ID
+    visitor_name = models.CharField(max_length=100)
+    visitor_email = models.EmailField()
+    category = models.CharField(max_length=50)
+    appointment_date = models.DateField()
+    appointment_time = models.TimeField()
+    reason = models.TextField()
+    designated_attendee = models.CharField(max_length=100)
 
-# Create your models here.
+    def _str_(self):
+        return self.visitor_name
+
--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -74,8 +76,11 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER' : 'postgres',
+        'PASSWORD': 'hari@1508',
+        'HOST' : 'localhost' 
     }
 }
 
