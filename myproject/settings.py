b0a73bebd21c6591291bbe7e8622bb8c9186345a
Hariharan P <pchari217@gmail.com>
2025-07-25 15:35:05 +0530

    first commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -76,11 +76,8 @@ WSGI_APPLICATION = 'myproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django',
-        'USER' : 'postgres',
-        'PASSWORD': 'hari@1508',
-        'HOST' : 'localhost' 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
